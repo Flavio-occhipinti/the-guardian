fcaa4e95206b84e8444100086858cfaa40cfd6e7
flavio <flavio.occhipinti.96@gmail.com>
2021-10-24 00:54:59 +0200

    add build folder

---
+++ build/precache-manifest.754456d1132b57bbe15c53d371dcd41f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642dd25112eddb87f49433f48a60b187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "324e9db7bd3f24174642",
+    "url": "/static/css/main.a75f2def.chunk.css"
+  },
+  {
+    "revision": "a24296229c06d65615b3",
+    "url": "/static/js/2.12f6a3f8.chunk.js"
+  },
+  {
+    "revision": "a2beac711f4bd397a58b9342874a1955",
+    "url": "/static/js/2.12f6a3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324e9db7bd3f24174642",
+    "url": "/static/js/main.19761b0a.chunk.js"
+  },
+  {
+    "revision": "2551d413c57a88f7e119",
+    "url": "/static/js/runtime-main.dfa9a1a5.js"
+  },
+  {
+    "revision": "b2c8dc8014997a7c2ea6c1cb1c67ae4e",
+    "url": "/static/media/icons.b2c8dc80.ttf"
+  },
+  {
+    "revision": "c3d900c51608d63db93ee91593dc449f",
+    "url": "/static/media/icons.c3d900c5.svg"
+  },
+  {
+    "revision": "cee58c3d5d7f000f87f7a988103e5455",
+    "url": "/static/media/icons.cee58c3d.woff"
+  },
+  {
+    "revision": "f8a6a60b057c3cb836ac8bfa34bed444",
+    "url": "/static/media/icons.f8a6a60b.eot"
+  }
+]);
