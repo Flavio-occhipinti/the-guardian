fe667f6fb311c1fac0b515a6ef237133ed1ac903
flavio <flavio.occhipinti.96@gmail.com>
2021-10-24 11:17:04 +0200

    static-website

--- src/shared/types/articles-state.d.ts
+++
@@ -1,9 +0,0 @@
-export type AppState = {
-  modal: {
-    title?: string;
-    active: boolean;
-    content?: any;
-    className?: string;
-    size?: "small";
-  };
-};
--- src/shared/types/store.d.ts
+++
@@ -1,7 +0,0 @@
-import { ArticlesState } from "./app-state";
-import { AppState } from "./articles-state";
-
-export type Store = {
-  app: AppState;
-  articles: ArticlesState;
-};
